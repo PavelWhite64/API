c6bc58f5d2d77189ad34654c44c850d441284db8
Pavel <Test@email.ru>
2023-05-17 14:03:57 +0400

    add object

--- src/test/java/ru/netology/rest/AllTests.java
+++ src/test/java/ru/netology/rest/AllTests.java
@@ -114,4 +114,24 @@ public class AllTests {
                 .then()
                 .statusCode(200);
     }
+    @Test
+    void shouldReturnDemoAccounts7() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[1].currency", equalTo("USD"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+        ;
+    }
 }
